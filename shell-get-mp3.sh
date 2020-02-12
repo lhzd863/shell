07b78257bdee859e69603f0e5f47afd62f531e5c
lhzd863 <zdwst618@126.com>
2020-02-13 00:30:29 +0800

    Update shell-get-mp3.sh

--- shell-get-mp3.sh
+++ shell-get-mp3.sh
@@ -2,7 +2,7 @@ cur=1
 while [ $cur -lt 1118 ]
 do
    curv=`printf "%03d" $cur`
-   if [ -f $curv.mp3 ];then
+   if [ ! -f $curv.mp3 ];then
        wget http://pse.tt56w.com:8000/单田芳/单田芳_白眉大侠320清晰/5tps.com_单田芳_白眉大侠_$curv.mp3?10103851687289x1581524244x10103857817949-316df399fa27d72058009a29f7f98f56?3
        mv "5tps.com_单田芳_白眉大侠_$curv.mp3?10103851687289x1581524244x10103857817949-316df399fa27d72058009a29f7f98f56?3" $curv.mp3
    fi
